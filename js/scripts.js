347472ac66ec1f922f64c47e980bf9e396c4b0b6
Tanner Heffner & Kyle Giard-Chase <student@epicodus.com>
2015-04-13 12:32:10 -0700

    begin adding functionality for marking tasks as complete

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 
     var newTask = { name: inputName, notes: inputNotes, duedate: inputDueDate };
 
-    $("ul#tasks").append("<li><span class='task'>" + newTask.name + "</span></li>");
+    $("ul#tasks").append("<li><span class='task'" + "id='" + newTask.name + "'>" + newTask.name + "</li><input type='checkbox'" + "id='" + newTask.name + "'></span>");
 
     $("input#new-name").val("");
     $("input#new-notes").val("");
@@ -19,6 +19,12 @@ $(document).ready(function() {
       $("#show-task h2").text(newTask.name);
       $(".notes").text(newTask.notes);
       $(".due-date").text(newTask.duedate);
+
+    });
+
+    $('#' + $(this).name).click(function() {
+      $("#complete").show();
+      $("ul#finished").append("<li><span class='task'>" + newTask.name + "</span></li>");
 
     });
   });
